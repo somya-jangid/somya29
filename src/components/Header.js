2781cfdeed8c418fb97ffa770baf54a35bc29993
somya-jangid <somyajangid78@gmail.com>
2025-02-24 02:36:19 +0530

    Add or update src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,1 @@
+import React from 'react';function Header() {return (<header className="header"><div className="logo">Netflix</div><nav><ul><li>Home</li><li>TV Shows</li><li>Movies</li><li>New & Popular</li><li>My List</li></ul></nav><div className="profile">Profile</div></header>);}export default Header;
