40a7b87f10660091a2026bcc2e12b9dacf56ea5c
somya-jangid <somyajangid78@gmail.com>
2025-02-24 02:36:16 +0530

    Add or update src/App.js

---
+++ src/App.js
@@ -0,0 +1,1 @@
+import React from 'react';import Header from './components/Header';import Footer from './components/Footer';function App() {return (<div className="App"><Header /><main>{/* Main content goes here */}</main><Footer /></div>);}export default App;
